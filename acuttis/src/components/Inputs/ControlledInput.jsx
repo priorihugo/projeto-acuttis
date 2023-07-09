8f9aefb35cdc6b44246897930539e4ce2be8e8cb
Hugo Amoglia Priori <priori.hugo@estudante.ufjf.br>
2023-07-09 18:05:56 -0300

    valida input para numero positivo

--- acuttis/src/components/Inputs/ControlledInput.jsx
+++ acuttis/src/components/Inputs/ControlledInput.jsx
@@ -1,13 +1,24 @@
-import { Controller } from "react-hook-form";
+const validatePositiveNumber = (value) => {
+  console.log("value ", value);
+
+  if (Number(value) > 0) {
+    return true;
+  } else {
+    return "Por favor, insira um número positivo.";
+  }
+};
 
 function ControlledInput({ register, errors, name, label, type }) {
   return (
     <div className="input-field col s6">
       <input
-        {...register(name, { required: "Campo obrigatório"  } )}
+        {...register(name, {
+          required: "Campo obrigatório",
+          validate: (v) => parseInt(v) > 0 || "Número deve ser positivo",
+        })}
         type={type}
       />
-      <label for={"val_dia"}>{label}</label>
+      <label htmlFor={"val_dia"}>{label}</label>
       <span className="helper-text red-text text-darken-3 " data-error="wrong">
         {errors[name]?.message}
       </span>
